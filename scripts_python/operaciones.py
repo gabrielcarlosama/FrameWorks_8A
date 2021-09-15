171d698d4820e3b1b10bd10d2d850017d3afeaae
gabrielcarlosama <lcarlosama32@gmail.com>
2021-09-15 00:27:44 -0500

    Carga inicial de los scripts python math_oper y suma

---
+++ scripts_python/operaciones.py
@@ -0,0 +1,8 @@
+a = 5
+b = 10
+suma = a+ b
+resta = a-b
+mult = a * b
+print("The addition is: ", suma)
+print("The subst is: ", resta)
+print("The mult is: ", mult)
---
+++ scripts_python/suma.py
@@ -0,0 +1,4 @@
+a = 5
+b = 10
+suma = a+ b
+print("The addition is: ", suma)
