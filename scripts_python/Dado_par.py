8ee9b145dd1dd9fddf62ab49205a146850afc02c
gabrielcarlosama <lcarlosama32@gmail.com>
2021-09-21 01:35:27 -0500

    Dados completos y modificados

--- scripts_python/Dado_par.py
+++ scripts_python/Dado_par.py
@@ -24,3 +24,31 @@ def dices() :
 
 #Main
 dices()
+import random
+
+
+def main():
+    print("DADOS IGUALES")
+    numero = int(input("Número de dados: "))
+    if numero < 2:
+        print("¡Imposible!")
+    else:
+        print("Dados: ", end="")
+        repetido = False
+        anterior = random.randrange(1, 7)
+        print(f"{anterior} ", end="")
+        for _ in range(1, numero):
+            dado = random.randrange(1, 7)
+            print(f"{dado} ", end="")
+            if dado == anterior:
+                repetido = True
+            anterior = dado
+        print()
+        if repetido:
+            print(f"El jugador ha perdido.")
+        else:
+            print(f"El jugador ha ganado.")
+
+
+if __name__ == "__main__":
+    main()
