37d2e84320e3bfa32188eac14a6f00b06e993bfa
gabrielcarlosama <lcarlosama32@gmail.com>
2021-10-08 22:58:59 -0500

    update calculadora

---
+++ scripts_python/Calculadora.py
@@ -0,0 +1,13 @@
+#CALCULADORABASICA
+a = 3
+b= 20
+
+suma=a+b
+resta=a-b
+multi=a*b
+divi=a/b
+
+print("LA SUMA ES ", suma)
+print("LA RESTA ES ", resta)
+print("LA MULTIPLICACION ES ", multi)
+print("LA DIVICION ES ", divi)
